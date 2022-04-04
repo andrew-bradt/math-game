6f0564f9915d6f881d236ab9687c6871829b5f4d
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-04 14:30:43 -0400

    add handle_answer method to Turn

--- entities/turn.rb
+++ entities/turn.rb
@@ -6,13 +6,22 @@ class Turn
   def begin(question)
     puts 'PLACEHOLDER FOR BEGIN'
   end
+  
+  # private
+  
+  attr_reader :players
 
   def handle_answer(is_correct, player)
-    puts 'PLACEHOLDER FOR HANDLE_ANSWER'
+    if is_correct
+      puts "#{player.name}: YES! You are correct."
+    else
+      puts "#{player.name}: Seriously? No!"
+      player.reduce_lives
+    end
   end
 
   def display_score
     puts 'PLACEHOLDER FOR DISPLAY_SCORE'
   end
-    
+  
 end
